8d6daa87691bf2858c7e309e5f50e804a7a7cdc4
Sarmad Hashmi <shash034@uottawa.ca>
2015-11-21 04:08:48 -0500

    Added instructor operations and corresponding views

---
+++ SEG3102Final/app/src/main/java/com/sarmad/uni/seg3102final/SetupParametersActivity.java
@@ -0,0 +1,11 @@
+package com.sarmad.uni.seg3102final;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupParametersActivity extends Activity {
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.setup_parameters);
+    }
+}
---
+++ SEG3102Final/app/src/main/java/com/sarmad/uni/seg3102final/VisualizeStudentTeamsActivity.java
@@ -0,0 +1,11 @@
+package com.sarmad.uni.seg3102final;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisualizeStudentTeamsActivity extends Activity {
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.visualize_students);
+    }
+}
